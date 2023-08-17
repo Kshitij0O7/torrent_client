674534d82db653bf617084b688f9c9d3ae485f74
Kshitij <kshitij.mahajan.20042@iitgoa.ac.in>
2023-08-17 23:43:43 +0530

    Modular import implementation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //'use strict';
-const fs = require('fs');
-const bencode = require('bencode');
+import * as fs from 'fs';
+import bencode from 'bencode';
+//import bencode from 'bencode'
 
 const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
 console.log(torrent.announce.toString('utf8'));
